a1575d3035583e96077d4e7a2b0cbb2c0132f444
stkisengese <skisengese@outlook.com>
2025-08-28 09:40:08 +0300

    feat: traverse recursively in the DOM tree

--- get_urls.go
+++ get_urls.go
@@ -8,7 +8,6 @@ import (
 	"golang.org/x/net/html"
 )
 
-
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
 	// Parse the html body into a string
 	doc, err := html.Parse(strings.NewReader(htmlBody))
@@ -22,4 +21,24 @@ func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
 		return nil, fmt.Errorf("invalid base URL: %w, err")
 	}
 	return traverse(doc, base), nil
+}
+
+func traverse(node *html.Node, base *url.URL) []string {
+	var urls []string
+	if node.Type == html.ElementNode && node.Data == "a" {
+		for _, attr := range node.Attr {
+			if attr.Key == "href" {
+				link, err := url.Parse(attr.Val)
+				if err == nil {
+					absLink := base.ResolveReference(link)
+					urls = append(urls, absLink.String())
+				}
+			}
+		}
+	}
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		traverse(child, base)
+	}
+	return urls
 }
