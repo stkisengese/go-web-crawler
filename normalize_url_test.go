4300f86da9f074a0f0eadde881d2edc27fe4df8f
stkisengese <skisengese@outlook.com>
2025-08-28 04:13:55 +0300

    refactor: improve test structure and re-enable invalid URL test case

--- normalize_url_test.go
+++ normalize_url_test.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestNormalizeURL(t *testing.T) {
 	tests := []struct {
-		name     string
-		inputURL string
-		expected string
+		name          string
+		inputURL      string
+		expected      string
+		errorContains string
 	}{
 		{
-			name: "remove scheme",
+			name:     "remove scheme",
 			inputURL: "http://blog.boot.dev/path",
 			expected: "blog.boot.dev/path",
 		},
@@ -30,18 +32,18 @@ func TestNormalizeURL(t *testing.T) {
 		// 	inputURL: "http://BLOG.boot.dev/path/",
 		// 	expected: "blog.boot.dev/path",
 		// },
-		// {
-		// 	name:          "handle invalid URL",
-		// 	inputURL:      `:\\invalidURL`,
-		// 	expected:      "",
-		// 	errorContains: "couldn't parse URL",
-		// },
+		{
+			name:          "handle invalid URL",
+			inputURL:      `:\\invalidURL`,
+			expected:      "",
+			errorContains: "couldn't parse URL",
+		},
 	}
 
 	for i, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			actual, err := normalizeURL(tc.inputURL)
-			if err != nil {
+			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
 				t.Errorf("Test %v - '%s' Fail: unexpected error: %v", i, tc.name, err)
 			}
 			if actual != tc.expected {
