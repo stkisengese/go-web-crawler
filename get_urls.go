92a5c500ab5e0fbb5f7eab53a6ad00e77bfc1008
stkisengese <skisengese@outlook.com>
2025-08-28 09:39:07 +0300

    feat: parse html content and base urls

--- get_urls.go
+++ get_urls.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
 
 func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	return []string{}, nil
+	// Parse the html body into a string
+	doc, err := html.Parse(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse HTML: %w", err)
+	}
+
+	// Parse the base URL link for solving relative URLs
+	base, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("invalid base URL: %w, err")
+	}
+	return traverse(doc, base), nil
 }
