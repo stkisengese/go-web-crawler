58e7001e89207c3a85c5ebcd2c0a13e037c4d519
stkisengese <skisengese@outlook.com>
2025-08-28 10:30:37 +0300

    feat: add CLI checks

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	BASE_URL := os.Args[1]
+	fmt.Println("Starting crawl of:", BASE_URL)
+
 }
