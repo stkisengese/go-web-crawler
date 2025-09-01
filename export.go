38856301136be3eb6132bb21d5fbfa676c6bce68
stkisengese <skisengese@outlook.com>
2025-09-01 17:25:16 +0300

    refactor: replace the loop with Index() method

--- export.go
+++ export.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 // extractDomain extracts the domain from a given URL string. If the URL is empty, it returns an empty string.
 // It iterates through the characters of the URL and returns the substring
 // up to the first '/' character. If no '/' is found, it returns the entire URL.
@@ -8,11 +10,10 @@ func extractDomain(url string) string {
 		return ""
 	}
 
-	for i, char := range url {
-		if char == '/' {
-			return url[:i]
-		}
+	if slashIndex := strings.Index(url, "/"); slashIndex != -1 {
+		return url[:slashIndex]
 	}
+
 	return url
 }
 
